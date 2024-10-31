bfd9e332b47ae813a54c51d57f24e0a1e9c526cd
akira2709 <145766714+akira2709@users.noreply.github.com>
2024-10-31 15:14:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI, Body
+from starlette.responses import JSONResponse
 
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+async def main():
+  return JSONResponse({'data': 'ok'}, status_code=200)
+
+
+host = 5000
+port = '127.0.0.1'
+uvicorn.run(app, host=host, port=port)
